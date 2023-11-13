5bd461e899a956b64df27b7387420427e3b7dcb3
Burke Libbey <burke.libbey@shopify.com>
2023-11-13 15:12:53 -0500

    Pass --no-gpg-sign when creating git commit for test
    Should have no effect for users without default signing, but not passing
    it creates a sort of weird experience for users with commit signing on
    by default.

--- test/resolve_test.rb
+++ test/resolve_test.rb
@@ -562,7 +562,7 @@ GEMSPEC
         export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME" GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
         git -c init.defaultBranch=main init >/dev/null &&
         git add shush.gemspec >/dev/null &&
-        git commit -m initial >/dev/null &&
+        git commit --no-gpg-sign -m initial >/dev/null &&
         git branch my-branch >/dev/null &&
         git rev-parse HEAD`.chomp
       raise "Failed to create git repo" if sha.empty? || !$?.success?
