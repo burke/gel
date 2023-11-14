c1d4bdf9c777c7278fd603786a0489ce5df4d061
Burke Libbey <burke.libbey@shopify.com>
2023-11-14 16:38:55 -0500

    lock opts

--- lib/gel/command/lock.rb
+++ lib/gel/command/lock.rb
@@ -1,44 +1,48 @@
 # frozen_string_literal: true
 
 class Gel::Command::Lock < Gel::Command::Base
-  # define_options do |o|
-  #   o.banner = <<~BANNER.chomp
-  #     TODO
-  #
-  #     Usage: gel lock
-  #
-  #     Options:
-  #   BANNER
-  # end
+  define_options do |o|
+    o.banner = <<~BANNER.chomp
+      TODO
 
-  # TODO: integrate arguments
-  def run(command_line)
-    options = {}
+      Usage: gel lock [<lockfile>] [<override>...]
+
+      Mode selection options:
+    BANNER
+    o.bool "--hold", "Don't bump any versions (default)"
+    o.bool "--patch", "Bump only patch versions"
+    o.bool "--minor", "Bump patch and minor versions"
+    o.bool "--major", "Bump patch, minor, and major versions"
+    o.separator("\nOther options:")
+    o.bool "--strict", "Use strict version constraints"
+    o.string "--lockfile", "Specify a lockfile to use"
+  end
+
+  def call(opts)
+    env_options = {}
 
     mode = :hold
-    strict = false
+    mode = :patch if opts[:patch]
+    mode = :minor if opts[:minor]
+    mode = :major if opts[:major]
+
+    env_options[:lockfile] = opts[:lockfile] || opts.arguments.shift
+
     overrides = {}
 
-    until command_line.empty?
-      case argument = command_line.shift
-      when "--strict"; strict = true
-      when "--major"; mode = :major
-      when "--minor"; mode = :minor
-      when "--patch"; mode = :patch
-      when "--hold"; mode = :hold
-      when /\A--lockfile(?:=(.*))?\z/
-        options[:lockfile] = $1 || command_line.shift
+    opts.arguments.each do |arg|
+      case arg
       when /\A((?!-)[A-Za-z0-9_-]+)(?:(?:[\ :\/]|(?=[<>~=]))([<>~=,\ 0-9A-Za-z.-]+))?\z/x
         overrides[$1] = Gel::Support::GemRequirement.new($2 ? $2.split(/\s+(?=[0-9])|\s*,\s*/) : [])
       else
-        raise "Unknown argument #{argument.inspect}"
+        raise "invalid override arugment"
       end
     end
 
-    options[:preference_strategy] = lambda do |gem_set|
-      Gel::PubGrub::PreferenceStrategy.new(gem_set, overrides, bump: mode, strict: strict)
+    env_options[:preference_strategy] = lambda do |gem_set|
+      Gel::PubGrub::PreferenceStrategy.new(gem_set, overrides, bump: mode, strict: opts[:strict])
     end
 
-    Gel::Environment.write_lock(output: $stderr, **options)
+    Gel::Environment.write_lock(output: $stderr, **env_options)
   end
 end
